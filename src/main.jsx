5fcfa5dc5d2522efb83c63f875789697cd786e64
GuiOtavioFatec <guilherme.santos85@aluno.cps.sp.gov.br>
2026-09-23 10:42:16 -0300

    teste 4

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,11 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
  <StrictMode>
+<div>
+  <header>
+    <h1>Guilherme Otavio Dos Santos</h1>
+  </header>
+</div>
  <BrowserRouter>
  <App />
  </BrowserRouter>
